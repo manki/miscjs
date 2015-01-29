c61c9c5e786fad7d6e4d3d513ea610414115a7b4
Muthu Kannan <m@manki.in>
2015-01-28 20:46:59 -0800

    fix some crazy stupid bugs.

--- xhr.js
+++ xhr.js
@@ -1,28 +1,55 @@
+if (!window.manki) { window.manki = {}; }
+if (!manki.miscjs) { manki.miscjs = {}; }
+
+
 /**
  * A simple XHR library.
  * Requires RSVP -- https://github.com/tildeio/rsvp.js/
+ *
+ * @param responseType Expected response type. See responseType at
+ *     https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#Properties
+ *     for valid values.
+ *
+ * @constructor
  */
-manki.miscjs.Xhr = function() {
+manki.miscjs.Xhr = function(responseType) {
+  this.responseType = responseType;
+};
+
+
+/**
+ * Creates an Xhr object for making JSON requests.
+ */
+manki.miscjs.Xhr.json = function() {
+  return new manki.miscjs.Xhr('json');
+};
+
+
+/**
+ * Creates an Xhr object for making plain text requests.
+ */
+manki.miscjs.Xhr.text = function() {
+  return new manki.miscjs.Xhr('text');
 };
 
 
 /**
  * Sends a GET request to {@code url} and returns an RSVP promise to the
  * response.
- * @param params An object or map of URL query parameters to pass.
+ * @param opt_params An object or map of URL query parameters to pass.
  */
-manki.miscjs.Xhr.prototype.get = function(url, params) {
-  return this.send_('GET', url, params);
+manki.miscjs.Xhr.prototype.get = function(url, opt_params) {
+  return this.send_('GET', url, opt_params);
 };
 
 
 /**
  * Sends a POST request to {@code url} and returns an RSVP promise to the
  * response.
- * @param params An object or map of URL query parameters to pass.
+ * @param opt_params An object or map of URL query parameters to pass.
  */
-manki.miscjs.Xhr.prototype.post = function(url, params) {
-  return this.send_('POST', url, params);
+manki.miscjs.Xhr.prototype.post = function(url, opt_params) {
+  return this.send_('POST', url, opt_params);
 };
 
 
@@ -30,11 +57,11 @@ manki.miscjs.Xhr.prototype.post = function(url, params) {
  * Does the actual XHR.
  * @private
  */
-manki.miscjs.Xhr.prototype.send_ = function(method, url, params) {
-  var thisController = this;
+manki.miscjs.Xhr.prototype.send_ = function(method, url, opt_params) {
+  var thisXhr = this;
   return new RSVP.Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+    xhr.responseType = thisXhr.responseType;
     xhr.onload = function() {
       if (this.status === 200) {
         resolve(this.response);
@@ -46,14 +73,54 @@ manki.miscjs.Xhr.prototype.send_ = function(method, url, params) {
     if (method === 'GET' || method === 'HEAD') {
       var element = document.createElement('a');
       element.href = url;
-      if (element.search && params) {
+      if (element.search && opt_params) {
         throw new Error('Cannot add params to a URL with query string');
       }
-      xhr.open(method, url + '?' + thisController.asQueryString_(params), true);
+      var fullUrl = opt_params ?
+          url + '?' + thisXhr.asQueryString_(opt_params) :
+          url;
+      xhr.open(method, fullUrl, true);
       xhr.send();
     } else {
       xhr.open(method, url, true);
-      xhr.send(thisController.asFormData_(params));
+      xhr.send(thisXhr.asFormData_(opt_params));
     }
   });
+};
+
+/**
+ * Makes URL query string from data in opt_params.
+ * @param opt_params An object or map of URL query parameters to pass.
+ * @private
+ */
+// Source: https://stackoverflow.com/a/1714899/13326
+manki.miscjs.Xhr.prototype.asQueryString_ = function(opt_params) {
+  var qsParts = [];
+  if (opt_params) {
+    for (p in opt_params) {
+      if (opt_params.hasOwnProperty(p)) {
+        qsParts.push(
+            encodeURIComponent(p) + '=' + encodeURIComponent(opt_params[p]));
+      }
+    }
+  }
+  return qsParts.join('&');
+};
+
+
+/**
+ * Makes FormData object with data in opt_params.
+ * @param opt_params An object or map of URL query parameters to pass.
+ * @private
+ */
+manki.miscjs.Xhr.prototype.asFormData_ = function(opt_params) {
+  var form = new FormData();
+  if (opt_params) {
+    for (p in opt_params) {
+      if (opt_params.hasOwnProperty(p)) {
+        form.append(p, opt_params[p]);
+      }
+    }
+  }
+  return form;
 };
